b1c0678c33f65a4c6e40772329a639214dd8763c
Dirk Ulbricht <dirk.ulbricht@gmail.com>
2014-11-28 21:25:56 +0100

    sss

--- Html_to_plaintxt.R
+++ Html_to_plaintxt.R
@@ -29,9 +29,16 @@ gettext<-function(input){
         plainhtml_short=plainhtml[(txt_start+1):(txt_end-1)]
         
         txt_s<-convert_html_to_text(plainhtml_short)
+
+               
+        
         txt<-paste(txt_s,sep='',collapse='')
-        txt<-gsub('»|,|\\.|:|<|>|\\n|"|[0-9]{1,20}|;|-|«|\\)|\\(|\\?',' ',txt)
+        txt<-gsub('\u0084|\u0093|\u0096',' ',txt)
+             
+        txt<-gsub('»|,|\\.|:|<|>|\\n|"|[0-9]{1,20}|;|-|«|\\)|\\(|\\?|( (\t)* )',' ',txt)
+        
         txt<-gsub('( ){2,}',' ',txt)# superfluos spaces eliminated
+   
         return(txt)
 }
 
@@ -45,26 +52,26 @@ library(XML)
 listsubdirs=list.files(DirRawTexts)
 
 for (subd in listsubdirs[1:1281]){
-# subd="2007.49"       
-listfiles=list.files(paste(DirRawTexts,'/',subd,sep='')) 
-if (length(grep('article',listfiles))!=0){
-        listfiles=listfiles[-grep('article',listfiles)]       
-}
-if (length(grep('plaintxt',listfiles))!=0){
-        listfiles=listfiles[-grep('plaintxt',listfiles)]       
-}
-if (length(grep('Ergebnis',listfiles))!=0){
-        listfiles=listfiles[-grep('Ergebnis',listfiles)]       
-}
-for (file in listfiles){
-        write.csv(gettext(paste(DirRawTexts,'/',subd,'/',file,sep=''))
-                  ,paste(DirRawTexts,'/',subd,'/','plaintxt-',file,sep=''),eol=''
-                  ,row.names=F#,col.names=F
-                  ,fileEncoding='UTF-8'
-                  ,quote=F# only possible as there are no commas left
-        )
-        
-}
+        # subd="1990.1"       
+        listfiles=list.files(paste(DirRawTexts,'/',subd,sep='')) 
+        if (length(grep('article',listfiles))!=0){
+                listfiles=listfiles[-grep('article',listfiles)]       
+        }
+        if (length(grep('plaintxt',listfiles))!=0){
+                listfiles=listfiles[-grep('plaintxt',listfiles)]       
+        }
+        if (length(grep('Ergebnis',listfiles))!=0){
+                listfiles=listfiles[-grep('Ergebnis',listfiles)]       
+        }
+        for (file in listfiles){
+                write.csv(gettext(paste(DirRawTexts,'/',subd,'/',file,sep=''))
+                          ,paste(DirRawTexts,'/',subd,'/','plaintxt-',file,sep=''),eol=''
+                          ,row.names=F#,col.names=F
+                          ,fileEncoding='UTF-8'
+                          ,quote=F# only possible as there are no commas left
+                )
+                
+        }
 }
 
 
