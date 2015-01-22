32ae4bd53ff5162f6eeb8252a985da7143bc5690
Dirk Ulbricht <dirk.ulbricht@gmail.com>
2015-01-22 12:07:48 +0100

    sentiment.R mit punctuation removal, zukunftszeitbezug angefangen

--- sentiment.R
+++ sentiment.R
@@ -1,12 +1,14 @@
-sentiment<-function (text, valueword) 
+function (text, valueword) 
 {
     if (length(text) == 2 & text[1] == ",x") {
         text = text[2]
     }
+    text = gsub("[[:punct:]]", "", text)
     text.split = sapply(strsplit(text, " "), function(x) x)
     ind = valueword[, 1] %in% text.split
     valdf = valueword[ind, , drop = F]
     valdf$h = sapply(valueword[ind, 1], function(x) sum(text.split %in% 
         x))
-    return(valdf)
+    nwords = length(text.split)
+    return(list(valdf, nwords))
 }
